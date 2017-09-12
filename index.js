5dab84a05d9694091aefa63e938a03ebae0644cc
Abigail Messmer <abbie.messmer@gmail.com>
2017-09-12 00:14:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const app = "I don't do much."
 function destructivelyAppendKitten(kitten){
   return kittens.push(kitten)
 }
+
+function destructivelyPrependKitten(kitten){
+  return kittens.unshift(kitten)
+}
